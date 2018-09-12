a57765fedc716fd92e8d7c169ed7226d3ce9c8da
Mark Wardell <mark.d.wardell@avanade.com>
2018-09-12 04:12:49 -0500

    All In

---
+++ Simple Calculator - StartingPoint/C#/SimpleCalculator/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace SimpleCalculator
+{
+    partial class frmCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnOne = new System.Windows.Forms.Button();
+            this.btnTwo = new System.Windows.Forms.Button();
+            this.btnDot = new System.Windows.Forms.Button();
+            this.btnEqual = new System.Windows.Forms.Button();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnSqrRoot = new System.Windows.Forms.Button();
+            this.btnByTwo = new System.Windows.Forms.Button();
+            this.btnByFour = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(26, 25);
+            this.txtInput.Margin = new System.Windows.Forms.Padding(6);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(382, 31);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            this.txtInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtInput_KeyPress);
+            // 
+            // btnOne
+            // 
+            this.btnOne.Location = new System.Drawing.Point(24, 75);
+            this.btnOne.Margin = new System.Windows.Forms.Padding(6);
+            this.btnOne.Name = "btnOne";
+            this.btnOne.Size = new System.Drawing.Size(68, 44);
+            this.btnOne.TabIndex = 1;
+            this.btnOne.Text = "1";
+            this.btnOne.UseVisualStyleBackColor = true;
+            // 
+            // btnTwo
+            // 
+            this.btnTwo.Location = new System.Drawing.Point(104, 75);
+            this.btnTwo.Margin = new System.Windows.Forms.Padding(6);
+            this.btnTwo.Name = "btnTwo";
+            this.btnTwo.Size = new System.Drawing.Size(68, 44);
+            this.btnTwo.TabIndex = 2;
+            this.btnTwo.Text = "2";
+            this.btnTwo.UseVisualStyleBackColor = true;
+            // 
+            // btnDot
+            // 
+            this.btnDot.Location = new System.Drawing.Point(104, 242);
+            this.btnDot.Margin = new System.Windows.Forms.Padding(6);
+            this.btnDot.Name = "btnDot";
+            this.btnDot.Size = new System.Drawing.Size(68, 44);
+            this.btnDot.TabIndex = 11;
+            this.btnDot.Text = ".";
+            this.btnDot.UseVisualStyleBackColor = true;
+            // 
+            // btnEqual
+            // 
+            this.btnEqual.Location = new System.Drawing.Point(186, 242);
+            this.btnEqual.Margin = new System.Windows.Forms.Padding(6);
+            this.btnEqual.Name = "btnEqual";
+            this.btnEqual.Size = new System.Drawing.Size(68, 44);
+            this.btnEqual.TabIndex = 12;
+            this.btnEqual.Text = "=";
+            this.btnEqual.UseVisualStyleBackColor = true;
+            this.btnEqual.Click += new System.EventHandler(this.btnEqual_Click);
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(264, 75);
+            this.btnPlus.Margin = new System.Windows.Forms.Padding(6);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(68, 44);
+            this.btnPlus.TabIndex = 13;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnSqrRoot
+            // 
+            this.btnSqrRoot.Location = new System.Drawing.Point(344, 131);
+            this.btnSqrRoot.Margin = new System.Windows.Forms.Padding(6);
+            this.btnSqrRoot.Name = "btnSqrRoot";
+            this.btnSqrRoot.Size = new System.Drawing.Size(68, 44);
+            this.btnSqrRoot.TabIndex = 18;
+            this.btnSqrRoot.Text = "√";
+            this.btnSqrRoot.UseVisualStyleBackColor = true;
+            this.btnSqrRoot.Click += new System.EventHandler(this.btnSqrRoot_Click);
+            // 
+            // btnByTwo
+            // 
+            this.btnByTwo.Location = new System.Drawing.Point(344, 187);
+            this.btnByTwo.Margin = new System.Windows.Forms.Padding(6);
+            this.btnByTwo.Name = "btnByTwo";
+            this.btnByTwo.Size = new System.Drawing.Size(68, 44);
+            this.btnByTwo.TabIndex = 19;
+            this.btnByTwo.Text = "½";
+            this.btnByTwo.UseVisualStyleBackColor = true;
+            this.btnByTwo.Click += new System.EventHandler(this.btnByTwo_Click);
+            // 
+            // btnByFour
+            // 
+            this.btnByFour.Location = new System.Drawing.Point(344, 242);
+            this.btnByFour.Margin = new System.Windows.Forms.Padding(6);
+            this.btnByFour.Name = "btnByFour";
+            this.btnByFour.Size = new System.Drawing.Size(68, 44);
+            this.btnByFour.TabIndex = 20;
+            this.btnByFour.Text = "¼";
+            this.btnByFour.UseVisualStyleBackColor = true;
+            this.btnByFour.Click += new System.EventHandler(this.btnByFour_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(182, 75);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 44);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "2";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // frmCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 302);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnByFour);
+            this.Controls.Add(this.btnByTwo);
+            this.Controls.Add(this.btnSqrRoot);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.btnEqual);
+            this.Controls.Add(this.btnDot);
+            this.Controls.Add(this.btnTwo);
+            this.Controls.Add(this.btnOne);
+            this.Controls.Add(this.txtInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmCalculator";
+            this.Text = "Simple Calculator";
+            this.Load += new System.EventHandler(this.frmCalculator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnOne;
+        private System.Windows.Forms.Button btnTwo;
+        private System.Windows.Forms.Button btnDot;
+        private System.Windows.Forms.Button btnEqual;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnSqrRoot;
+        private System.Windows.Forms.Button btnByTwo;
+        private System.Windows.Forms.Button btnByFour;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
